05ba9be3b2b43b0ed3290dd94af8038c80bdc97e
Max Rogers <maxrogerssepulveda@gmail.com>
2021-04-26 16:50:06 -0400

    basic layout in features

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,72 @@
 import React from "react";
+import icons from "../assets/img/sprite.svg";
 
 const Features = () => {
-  return <section className="features">Features</section>;
+  return (
+    <section className="features">
+      <div className="feature">
+        <svg className="feature__icon">
+          <use xlinkHref={`${icons}#icon-global`}></use>
+        </svg>
+        <h4 className="heading-4">World's best luxury homes</h4>
+        <p className="feature__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus.
+        </p>
+      </div>
+
+      <div className="feature">
+        <svg className="feature__icon">
+          <use xlinkHref={`${icons}#icon-trophy`}></use>
+        </svg>
+        <h4 className="heading-4">Only the best properties</h4>
+        <p className="feature__text">
+          Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus
+          rerum sed a eligendi aut quia.
+        </p>
+      </div>
+      <div className="feature">
+        <svg className="feature__icon">
+          <use xlinkHref={`${icons}#icon-map-pin`}></use>
+        </svg>
+        <h4 className="heading-4">All homes in top locations</h4>
+        <p className="feature__text">
+          Tenetur distinctio necessitatibus pariatur voluptatibus quidem
+          consequatur harum.
+        </p>
+      </div>
+      <div className="feature">
+        <svg className="feature__icon">
+          <use xlinkHref={`${icons}#icon-key`}></use>
+        </svg>
+        <h4 className="heading-4">New home in one week</h4>
+        <p className="feature__text">
+          Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor sit
+          amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div className="feature">
+        <svg className="feature__icon">
+          <use xlinkHref={`${icons}#icon-presentation`}></use>
+        </svg>
+        <h4 className="heading-4">Top 1% realtors</h4>
+        <p className="feature__text">
+          Quidem consequatur harum, voluptatum mollitia quae. Tenetur distinctio
+          necessitatibus pariatur voluptatibus.
+        </p>
+      </div>
+      <div className="feature">
+        <svg className="feature__icon">
+          <use xlinkHref={`${icons}#icon-lock`}></use>
+        </svg>
+        <h4 className="heading-4">Secure payments on nexter</h4>
+        <p className="feature__text">
+          Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia
+          quae.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
